617466b67a54a2db51a93df74269c4c9f13fdb83
Christopher Sikes <sikes.chris@gmail.com>
2012-06-18 12:07:41 -0400

    update

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.configure('production', function(){
 // ================================================
 // Start Server
 // ================================================
-app.listen(3000, function(){
+app.listen(80, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
 
