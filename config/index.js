d7021c40bd1cd615832fa1c9b0032bed954ff07a
Christopher Sikes <sikes.chris@gmail.com>
2012-06-18 12:00:54 -0400

    deploy update

---
+++ config/index.js
@@ -0,0 +1,11 @@
+module.exports = function(app){
+  return {
+    session:{
+      db:{
+        db:'QueenSession',
+        host: 'mongodb://nodejitsu:ad0e874a9c1ad670a0771209ad9ba98b@flame.mongohq.com:27087/nodejitsudb668732957824'
+      },
+      secret:"mysecret"
+    }
+  }
+}
